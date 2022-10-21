0d0fc62377618d47b3cd22acf2bf87b88eddf943
Harsh Raghavani <harshraghavani@Harshs-MacBook-Air.local>
2022-10-21 13:35:17 +0530

    Code Edited

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-try('300x200')
 Import tinter as tk
 Import linecache
 Import smtplib,ssl
@@ -8,3 +7,13 @@ Import os
 win=tk.Tk()
 win.configure(bg='black')
 win.geometry('450x500')
+win.title('CRYPTOGYAN')
+line=tk.PhotoImage(file='bg4.png')
+ln=tk.Label(win,image=line,bd=0)
+ln.pack()
+win.resizable(0,0)
+fine=tk.PhotoImage(file='logbg.png')
+tp=tk.PhotoImage(file='otp.png')
+Def login():
+    root=tk.Toplevel()
+    root.geometry('300x200')
