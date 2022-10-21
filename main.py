ea7a883e925bc5e054ab7036094c07d8014c40e0
Harsh Raghavani <harshraghavani@Harshs-MacBook-Air.local>
2022-10-21 13:41:29 +0530

    "code edited"

--- main.py
+++ main.py
@@ -17,3 +17,13 @@ tp=tk.PhotoImage(file='otp.png')
 Def login():
     root=tk.Toplevel()
     root.geometry('300x200')
+    root.title('Login')
+
+    
+    log=tk.Label(root,image=fine)
+    log.place(x=0,y=0)
+
+    usname=tk.Entry(root)
+    usname.place(x=120,y=87,height=25)
+    usname.configure(bd=2)
+
